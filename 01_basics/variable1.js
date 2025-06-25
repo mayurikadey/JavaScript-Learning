877acf1d75a236e139dee6aebef3418a05693d76
mayurikadey <mayurikagit@gmail.com>
2025-06-25 11:09:58 +0000

    variable tutorial

---
+++ 01_basics/variable1.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail= "mayusaila@google.com";
+var accountPassword = "12345";
+accountCity = "Hyderabad"
+let accountState;
+//accountId = 2                      // not allowed
+
+accountEmail="sailamayu@gmail.com";
+accountPassword="222222"
+accountCity="Kolkata"
+
+console.log(accountId);
+
+/*
+Prefer not to use Var, because of issue in block scope and functional scopes
+
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
