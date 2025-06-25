e6528e07493b60f90b05c8a1b14b7a29dc409ef7
mayurikadey <mayurikagit@gmail.com>
2025-06-25 13:37:21 +0000

    interim work

---
+++ 01_basics/conversionOperation2.js
@@ -0,0 +1,12 @@
+// --------------   Operation   -------------------- //
+
+let val = 3
+let negetiveValue = -val
+//console.log(negetiveValue)
+
+//console.table([(2+2),(2-1),(2*2),(2**2),(15/3),(2%3)])
+
+let str1 = "hello!"
+let str2 = " Mayurika"
+let str3 = str1 + str2
+console.log(str3)
