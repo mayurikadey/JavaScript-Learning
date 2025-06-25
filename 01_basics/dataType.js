7390885ad5dd434ec722ec8b1f6ad348d3c6f12b
mayurikadey <mayurikagit@gmail.com>
2025-06-25 11:36:03 +0000

    datatypes tutorial

---
+++ 01_basics/dataType.js
@@ -0,0 +1,28 @@
+"use strict";  //treat all Js code as newer version
+
+//alert (3 + 3)   //We are using node.js, not browser
+
+console.log (3
+    
+    +3)     // Code readability should be high
+console.log("Mayurika")
+
+let name = "Mayurika"
+let age = 21
+let isLoggedIn =  false
+
+//number => 2^53 (2 to the power 53)
+//bigint
+//string => "abcd"
+//boolean => true/false
+//null => standalone value
+//undefined => while value is not defined
+// symbol => unique
+
+
+//object
+
+console.log(typeof(false));
+console.log(typeof(age));
+console.log(typeof(null)); //object
+console.log(typeof(undefined)); //undefined
