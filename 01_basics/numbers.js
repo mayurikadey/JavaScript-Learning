9c04a93bfacdc919a6caefdc7fdc79b5255faaeb
mayurikadey <mayurikagit@gmail.com>
2025-06-26 14:27:16 +0000

    Number work

---
+++ 01_basics/numbers.js
@@ -0,0 +1,24 @@
+const score = 400
+console.log(score);  //400
+
+const balance = new Number(100.2658935)
+console.log(balance);   // [Number: 100]
+
+// console.log(balance.toString()); //100.2658935
+// console.log(balance.toFixed(2)); //100.27
+
+const otherNumber = 23.8966
+//console.log(otherNumber.toPrecision(4)); // 23.90 [String type]
+
+const otherNumber1 = 1235.9658
+// console.log(otherNumber1.toPrecision(3)); //1.24e+3
+// console.log(otherNumber1.toPrecision(4))  //1236
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString()); //American Standards
+console.log(hundreds.toLocaleString('en-In')) // Indian standards
+
+
+
+
+
