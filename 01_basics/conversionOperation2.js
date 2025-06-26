44761212767ab2558086568ad3a39156ea403708
mayurikadey <mayurikagit@gmail.com>
2025-06-26 11:14:21 +0000

    work done note

--- 01_basics/conversionOperation2.js
+++ 01_basics/conversionOperation2.js
@@ -9,6 +9,29 @@ let negetiveValue = -val
 let str1 = "hello!"
 let str2 = " Mayurika"
 let str3 = str1 + str2
-console.log(str3)
-console.log(typeof str3);
-console.log("Mayurika")
+// console.log(str3)
+// console.log(typeof str3);
+
+// console.log("1" + 2);        //Ans: 12
+// console.log(1 + "2");        //Ans: 12
+// console.log("1" + 2 + 2);    //Ans: 122
+// console.log(1 + 2 + "2");    //Ans: 32
+// console.log((3 + 4) * (5 % 3)) // 14
+
+// console.log(true); //true
+// console.log(+true); //1
+
+let n1, n2, n3
+n1 = n2 = n3 = 2+2  //not good way of writing code
+// console.log(n1);
+
+let gameCounter = 100
+gameCounter++;
+//console.log(gameCounter); // 101
+
+let gameCounter2= 100;
+++gameCounter2;
+console.log(gameCounter2);
+
+
+
